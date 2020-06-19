a61615a1b3a3587a449d85af894401f7e020cd9b
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-06-19 21:55:21 +1200

    Wrote code to format the excel table so that it shows what thread, logger and level are given on a date

--- src/main/java/nz/ac/vuw/swen301/a2/server/StatsXLSServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/StatsXLSServlet.java
@@ -1,12 +1,18 @@
 package nz.ac.vuw.swen301.a2.server;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.codehaus.plexus.util.FileUtils;
 import org.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -16,7 +22,111 @@ public class StatsXLSServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/vnd.ms-excel");
+
+        jsonLogs = testJSONLogs();
+        System.out.print(jsonLogs.size());
+        //jsonLogs = LogsServlet.jsonLogs;
+        ArrayList<String> dates = getDates();
+        ArrayList<String> loggers = getLoggers();
+        ArrayList<String> threads = getThreads();
+        ArrayList<String> warnings = new ArrayList<>();
+        warnings.add("ALL");
+        warnings.add("TRACE");
+        warnings.add("DEBUG");
+        warnings.add("INFO");
+        warnings.add("WARN");
+        warnings.add("ERROR");
+        warnings.add("FATAL");
+        warnings.add("OFF");
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Stats");
+
+        int rows = 0;
+        Row row = sheet.createRow(rows);
+        for(int i = 0; i< dates.size();i++) {
+            Cell cell = row.createCell((i+1));
+            cell.setCellValue(dates.get(i));
+        }
+        rows++;
+        for ( int i = 0; i <loggers.size(); i++) {
+            row = sheet.createRow(rows);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(loggers.get(i));
+            for(int j = 0; j< dates.size();j++) {
+                int count = 0;
+                for(JSONObject jsonObject : jsonLogs) {
+                    String timestamp = jsonObject.get("timestamp").toString().substring(0,10);
+                    String log = jsonObject.getString("logger");
+                    if(timestamp.equals(dates.get(j)) && log.equals(loggers.get(i))) {
+                        count++;
+                    }
+                }
+                Cell cell2 = row.createCell(j+1);
+                cell2.setCellValue(count);
+            }
+           rows++;
+        }
+
+        for ( int i = 0; i <warnings.size(); i++) {
+            row = sheet.createRow(rows);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(warnings.get(i));
+            for(int j = 0; j< dates.size();j++) {
+                int count = 0;
+                for(JSONObject jsonObject : jsonLogs) {
+                    String timestamp = jsonObject.get("timestamp").toString().substring(0,10);
+                    String level = jsonObject.getString("level");
+                    if(timestamp.equals(dates.get(j)) && level.equals(warnings.get(i))) {
+                        count++;
+                    }
+                }
+                Cell cell2 = row.createCell(j+1);
+                cell2.setCellValue(count);
+            }
+            rows++;
+        }
+
+        for ( int i = 0; i <threads.size(); i++) {
+            row = sheet.createRow(rows);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(threads.get(i));
+            for(int j = 0; j< dates.size();j++) {
+                int count = 0;
+                for(JSONObject jsonObject : jsonLogs) {
+                    String timestamp = jsonObject.get("timestamp").toString().substring(0,10);
+                    String thread = jsonObject.getString("thread");
+                    if(timestamp.equals(dates.get(j)) && thread.equals(threads.get(i))) {
+                        count++;
+                    }
+                }
+                Cell cell2 = row.createCell(j+1);
+                cell2.setCellValue(count);
+            }
+            rows++;
+        }
+
+
+        out.close();
+        try {
+            FileOutputStream outputStream = new FileOutputStream("statsTable.xls");
+            workbook.write(outputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        resp.setContentType("application/vnd.ms-excel");
+        resp.setHeader("Content-Disposition", "filename=\"statsTable.xls\"");
+        File srcFile = new File("statsTable.xls");
+
+        resp.setStatus(200);
+
+
     }
 
     public ArrayList<String> getDates() {
