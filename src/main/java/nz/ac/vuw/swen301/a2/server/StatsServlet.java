ad152aee621c5a3ca29402b521de392871224a0f
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-06-19 20:18:12 +1200

    Added helper methods from StatsCSVServet into StatsServet that are going to be useful

--- src/main/java/nz/ac/vuw/swen301/a2/server/StatsServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/StatsServlet.java
@@ -1,6 +1,53 @@
 package nz.ac.vuw.swen301.a2.server;
 
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class StatsServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    public ArrayList<String> getDates() {
+        ArrayList<String> dates = new ArrayList<String>();
+        for(JSONObject jsonObject : jsonLogs) {
+            String timestamp = jsonObject.get("timestamp").toString().substring(0,10);
+            boolean found = false;
+            if(!dates.contains(timestamp)) {
+                dates.add(timestamp);
+            }
+        }
+        return dates;
+    }
+
+    public ArrayList<String> getLoggers() {
+        ArrayList<String> loggers = new ArrayList<String>();
+        for(JSONObject jsonObject : jsonLogs) {
+            String log = jsonObject.getString("logger");
+            if(!loggers.contains(log)) {
+                loggers.add(log);
+            }
+        }
+        return loggers;
+    }
+
+    public ArrayList<String> getThreads() {
+        ArrayList<String> threads = new ArrayList<String>();
+        for(JSONObject jsonObject : jsonLogs) {
+            String thread = jsonObject.getString("thread");
+            if(!threads.contains(thread)) {
+                threads.add(thread);
+            }
+        }
+        return threads;
+    }
 }
