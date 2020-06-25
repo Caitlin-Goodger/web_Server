399c0fecb3c729255added88b8fbfd7f12b30e3f
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-06-25 21:01:22 +1200

    Added helper methods to get dates and test logs

--- src/main/java/nz/ac/vuw/swen301/a2/server/StatsPNGServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/StatsPNGServlet.java
@@ -1,15 +1,60 @@
 package nz.ac.vuw.swen301.a2.server;
 
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class StatsPNGServlet extends HttpServlet {
+
+    public static ArrayList<JSONObject> jsonLogs = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+    }
+
+    public ArrayList<String> getDates() {
+        ArrayList<String> dates = new ArrayList<String>();
+        for(JSONObject jsonObject : jsonLogs) {
+            String timestamp = jsonObject.get("timestamp").toString().substring(0,10);
+            boolean found = false;
+            if(!dates.contains(timestamp)) {
+                dates.add(timestamp);
+            }
+        }
+        return dates;
+    }
+    
+
+    public ArrayList<JSONObject> testJSONLogs() {
+        ArrayList<JSONObject> j = new ArrayList<>();
+
+        JSONObject j1 = new JSONObject();
+        j1.put("id", UUID.randomUUID().toString());
+        j1.put("level", "ALL");
+        j1.put("timestamp", "2019-07-29T09:12:33.001Z");
+        j1.put("thread","main");
+        j1.put("message", "help");
+        j1.put("logger", "logger");
+
+        j.add(j1);
+
+        JSONObject j2 = new JSONObject();
+        j2.put("id", UUID.randomUUID().toString());
+        j2.put("level", "DEBUG");
+        j2.put("timestamp", "2018-07-29T09:12:33.001Z");
+        j2.put("thread","main");
+        j2.put("message", "help");
+        j2.put("logger", "l");
+
+        j.add(j2);
+
+        return j;
     }
 }
