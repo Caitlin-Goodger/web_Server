53b7ebcba409d2de9d9e62ef7727eb92cc22c91b
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-06-25 20:58:50 +1200

    Mapped StatsPNGServlet to a servlet

--- src/main/java/nz/ac/vuw/swen301/a2/server/StatsPNGServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/StatsPNGServlet.java
@@ -1,4 +1,15 @@
 package nz.ac.vuw.swen301.a2.server;
 
-public class StatsPNGServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class StatsPNGServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
